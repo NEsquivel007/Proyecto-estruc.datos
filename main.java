e8991af1fed0d7024f0e7fca1a1a067c1600e4cf
NEsquivel007 <nesquivelm@ucenfotec.ac.cr>
2025-11-16 16:39:20 -0600

    Delete main.java

--- main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) throws Exception {
-        listaProductos inventario = new listaProductos();
-        Menu Menu = new Menu(inventario);
-        Menu.run();
-    }
-}
